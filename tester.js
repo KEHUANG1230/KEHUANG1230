580edec764fd543562a63aed1a793d9a6c8c083b
KEHUANG1230 <KEvinh368@nycstudents.net>
2025-02-24 08:54:03 -0500

    Update tester.js

--- tester.js
+++ tester.js
@@ -1,9 +1,8 @@
 class Tester {
   constructor(entityId, x, y, z, scale = 1, color = 'red') {
-    this.obj = document.getElementById(entityId); // Get the entity by ID
+    this.obj = document.getElementById(entityId);
     this.obj.setAttribute('position', `${x} ${y} ${z}`);
     this.obj.setAttribute('scale', `${scale} ${scale} ${scale}`);
     this.obj.setAttribute('material', `color: ${color}`);
-    //this.obj.setAttribute('dynamic-body', 'mass: 1');  // Add dynamic-body for physics
   }
 }
