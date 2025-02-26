8eabdc72505080c93c80226a8adbefbf5640793b
KEHUANG1230 <KEvinh368@nycstudents.net>
2025-02-26 17:22:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let rnd = (l,u) => Math.random()*(u-l) + l;
 let scene, entity, stamia_text,stamina_bar = 100, h=100;
-let wendigo = { template: "#wendigoModel", charge: "wendigo--Charge", scale: 0.8, speed: 0.2 }
+let wendigo = { template: "#wendigoModel", scale: 1.6, speed: 2.4 }
 let banana = false;
 window.onload = function(){
 	scene = document.querySelector("a-scene");
@@ -30,13 +30,18 @@ window.onload = function(){
 	blackScreen = document.getElementById("blackScreen");
 	dayLight = document.getElementById("dayLight");
 	floor = document.getElementById("floor");
-    //sky = document.getElementById("sky");
+    sky = document.getElementById("sky");
 	door2 = document.getElementById("door2");
+	whitehallway = document.getElementById("whitehallway");
+	hallwaydoor = document.getElementById("hallwaydoor");
+	hallwayMonster = document.getElementById("hallwaymonster");
+	thirdPersonCamera = document.getElementById("thirdPersonCamera");
+	
 	
    car1.addEventListener("click", function () {
     console.log("Car 1 clicked");
 
-    cameraRig.setAttribute("position", carCamera1.getAttribute("position"));
+    //cameraRig.setAttribute("position", carCamera1.getAttribute("position"));
     camera.setAttribute("camera", "active: false");
     carCamera1.setAttribute("camera", "active: true");
 
@@ -104,13 +109,13 @@ window.onload = function(){
         });
 		
 
-        //sky.setAttribute("animation__color", {
-          //property: "color",
-          //from: "#87CEEB",
-          //to: "#000033", 
-          //dur: 30000, 
-          //easing: "linear",
-        //});
+        sky.setAttribute("animation__color", {
+          property: "color",
+          from: "#87CEEB",
+          to: "#000033", 
+          dur: 30000, 
+          easing: "linear",
+        });
 		
       setTimeout(() => {
         timeText.setAttribute("visible", false);
@@ -118,6 +123,7 @@ window.onload = function(){
 		
 		
         camera.setAttribute("camera", "active: true");
+		camera.setAttribute("position","20, 1.6, 40");
         carCamera1.setAttribute("camera", "active: false");
       }, 4000);
     });
@@ -137,19 +143,15 @@ window.onload = function(){
             }
         })
 		
-    //for (let i = 0; i < 1; i++) {
-    //monster = new Monster(-5, 0, 0, wendigo);
-	
-	//monster = new Monster(68, 0, -34, wendigo);
-  //}
+    monster = new Monster(-5, 0, 0, wendigo);
 	
 	//let monster3 = new Tester('monster3', -5, 0, 0);
-	let monster1 = new Tester('monster1', 20, 4, 0);
-	let monster2 = new Tester('monster2', 14, 16, 0);
+	 monster1 = new Monster(20, 0, -4, wendigo);
+	 monster2 = new Monster(14, 16, 0, wendigo);
   
    //Create MonsterFollower instances to make them follow the camera
-  new MonsterFollower(camera, monster1.obj, 10, 0.35);
-  new MonsterFollower(camera, monster2.obj, 10, 0.35); 
+  //new MonsterFollower(camera, monster1.obj, 10, 0.35);
+  //new MonsterFollower(camera, monster2.obj, 10, 0.35); 
   //new MonsterFollower(camera, monster3.obj, 25, 0.32); 
   
  
@@ -271,7 +273,7 @@ window.onload = function(){
 	this.go = true;
   window.addEventListener("keydown", function(e){
 		if (e.key == "c"){
-			 camera.object3D.position.y = 0.5;
+			 cameraRig.object3D.position.y = 0.5;
 			 camera.setAttribute("wasd-controls","acceleration:25");
 			this.go = false;
 			this.allow = false;
@@ -279,7 +281,7 @@ window.onload = function(){
 		}
 	window.addEventListener("keyup", function(e){
 	  if (e.key == "c"){
-		camera.object3D.position.y = 1.6;
+		cameraRig.object3D.position.y = 1.6;
 		this.go = true;
 		this.allow = true;
 		camera.setAttribute("wasd-controls","acceleration:65");
@@ -301,13 +303,13 @@ window.onload = function(){
 	window.addEventListener("keypress",function(e){
 		if(e.key == "y"){
 			if (isFirstPress1){
-				camera.object3D.position.y = 0.35;
+				cameraRig.object3D.position.y = 0.35;
 				camera.setAttribute("wasd-controls","acceleration:25");
 				this.go = false;
 				this.allow = false;
 				console.log(e.key)
 			}else {
-				camera.object3D.position.y = 1.6;
+				cameraRig.object3D.position.y = 1.6;
 				camera.setAttribute("wasd-controls","acceleration:65");
 				this.allow = true;
 				this.go = true;
@@ -416,6 +418,20 @@ window.onload = function(){
 	door2.onclick = function(){
 	 if (banana) {
 		door2.setAttribute("rotation",{x:0, y:90, z:0});
+		
+    scene.setAttribute("visible", false);
+   whitehallway.setAttribute("visible", true);
+
+    camera.setAttribute("position", { x: 0, y: 1.6, z: 10 }); 
+    hallwaymonster.setAttribute("position", { x: 0, y: 0, z: -10 });
+
+    camera.setAttribute("camera", "active: false");
+	
+    thirdPersonCamera.setAttribute("camera", "active: true");
+    thirdPersonCamera.setAttribute("position", { x: 0, y: 2, z: 5 }); 
+    cameraRig.appendChild(thirdPersonCamera);
+
+    startHallwayChase();
 	}
 }
 	let isFirstPress = true;
@@ -429,6 +445,12 @@ window.onload = function(){
 			}
 			
 			isFirstPress = !isFirstPress;
+		}
+	})
+	
+	window.addEventListener("keypress", function(e){
+		if (e.key == "p"){
+			banana = true;
 		}
 	})
 	
@@ -521,34 +543,55 @@ function loop(){
 	stamia_text.setAttribute("value", `Stamina: ${stamina_bar}`);
 	health_text.setAttribute("value", `health: ${h}`);
 	
-	//for (let monster of monsters) {
-    //let d = distance(monster.obj, camera);
-    //if (3 < d && d < 6) {
-      //monster.rotateTowards(camera);
-      //monster.forward()
-    //} else {
-      //monster.stop();
-    //}
-//}
+  let monsters = [monster, monster1, monster2];
+
+
+monster.chaseDistance = 45; 
+monster1.chaseDistance = 12; 
+monster2.chaseDistance = 12; 
+
+for (let monster of monsters) {
+  let d = distance(monster.obj, camera);
+
+  if (d < 3.8) { 
+    if (!monster.isAttacking) {
+      monster.isAttacking = true;
+      monster.obj.setAttribute("animation-mixer", { clip: "wendigo_jjk_body1_model0_skeleton|Attack 3", loop: "once" });
+
+      setTimeout(() => {
+        h -= 10; 
+        if (h < 0) h = 0; 
+        monster.isAttacking = false;
+      }, 2200); 
+    }
+  } else if (d < monster.chaseDistance) { 
+    monster.stopWandering();
+    monster.rotateTowards(camera);
+    monster.forward();
+    monster.obj.setAttribute("animation-mixer", { clip: "wendigo_jjk_body1_model0_skeleton|Run", loop: "repeat" });
+  } else { 
+    monster.startWandering();
+  }
+}
   
-	let testingMonsters = [monster1, monster2];//, monster3]; 
+	//let testingMonsters = [monster1, monster2];//, monster3]; 
 	
-	for (Tester of testingMonsters){ 
-	let d = distance(monster1, camera);
-	let dx = distance(monster2, camera);
+	//for (Tester of testingMonsters){ 
+	//let d = distance(monster1, camera);
+	//let dx = distance(monster2, camera);
 	//let d3 = distance(monster3, camera);
 	
-    if (d < 1.5) { 
-      h -= 1;  
-      if (h < 0) h = 0;  
-    } else if (dx < 1.5){
-		h -= 1;
-		if (h < 0) h = 0;
+    //if (d < 1.5) { 
+      //h -= 1;  
+      //if (h < 0) h = 0;  
+    //} else if (dx < 1.5){
+		//h -= 1;
+		//if (h < 0) h = 0;
 	//}else if (d3 < 1.5){
 		//h -= 1;
 		//if (h < 0) h = 0;
-	}
-  }
+	//}
+  //}
 
 	window.requestAnimationFrame(loop);
 }
@@ -597,5 +640,69 @@ function teleportBanana() {
 }
 
 function teleportHouse() {
-	camera.setAttribute("position", {x:20, y: 0, z: 40});
+  camera.setAttribute("position", { x: 20, y: 1.6, z: 40 }); 
+
+}
+
+function startHallwayChase() {
+
+  hallwayMonster.setAttribute("animation", {
+    property: "position",
+    to: "0 0 10", 
+    dur: 5000, 
+    easing: "linear",
+  });
+
+  cameraRig.setAttribute("animation", {
+    property: "position",
+    to: "0 1.6 -50", 
+    dur: 5000,
+    easing: "linear",
+  });
+	
+	dayLight.setAttribute("animation__light", {
+          property: "light.intensity",
+          from: 0.2,
+          to: 1,
+          dur: 5000,
+          easing: "linear",
+        });
+		
+		
+		scene.setAttribute("animation__fog", {
+          property: "fog.color",
+          from: "#000000",
+          to: "#ffffff",
+          dur: 5000, 
+          easing: "linear",
+        });
+		
+		scene.setAttribute("animation__fogDensity", {
+          property: "fog.density",
+          from: 0.08,
+          to: 0.001,
+          dur: 5000, 
+          easing: "linear",
+        });
+		
+
+        sky.setAttribute("animation__color", {
+          property: "color",
+          from: "#000033",
+          to: "#87CEEB", 
+          dur: 5000, 
+          easing: "linear",
+        });
+		
+  setTimeout(() => {
+    hallwaydoor.setAttribute("rotation", { x: 0, y: 90, z: 0 });
+
+    camera.setAttribute("position", {x:-80, y: 1.6, z:485});
+
+   whitehallway.setAttribute("visible", false);
+    scene.setAttribute("visible", true);
+	
+    camera.setAttribute("camera", "active: true");
+    cameraRig.removeChild(thirdPersonCamera);
+  }, 5000);
 }
