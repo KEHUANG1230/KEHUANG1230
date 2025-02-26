c13264f0df20b14451606305cbe748357fd6b2de
KEHUANG1230 <KEvinh368@nycstudents.net>
2025-02-26 17:22:14 -0500

    Update monster.js

--- monster.js
+++ monster.js
@@ -1,35 +1,95 @@
-class Monster{
-  constructor(x,y,z,model){
+class Monster {
+  constructor(x, y, z, model) {
     this.model = model;
     this.obj = document.createElement("a-gltf-model");
-    this.obj.setAttribute("src",model.template);
-    this.obj.setAttribute("animation-mixer",{clip:model.charge,timeScale:0});
-    this.obj.setAttribute("scale",`${model.scale} ${model.scale} ${model.scale}`);
-    this.obj.setAttribute("position",{x:x,y:y,z:z})
-    this.obj.setAttribute("shadow",{receive: true});
+    this.obj.setAttribute("src", model.template);
+    this.obj.setAttribute("animation-mixer", { clip: "wendigo_jjk_body1_model0_skeleton|Idle", loop: "repeat" });
+    this.obj.setAttribute("scale", `${model.scale} ${model.scale} ${model.scale}`);
+    this.obj.setAttribute("position", { x: x, y: y, z: z });
+    this.obj.setAttribute("shadow", { receive: true });
     scene.append(this.obj);
-  }
-  angleTo(that){
-      let dx = that.object3D.position.x - this.obj.object3D.position.x;
-      let dz = that.object3D.position.z - this.obj.object3D.position.z;
 
-      this.angle = Math.atan(dx/dz)
-      if(dz < 0){
-          this.angle += Math.PI
-      }
+    this.chaseDistance = 10;
+    this.attackDistance = 2; 
+    this.speed = 0.3;
+    this.isChasing = false;
+    this.isAttacking = false;
+    this.wanderInterval = null;
   }
-  rotateTowards(that){
-      this.angleTo(that);
-      this.obj.object3D.rotation.y = this.angle;
+
+  angleTo(target) {
+    let dx = target.object3D.position.x - this.obj.object3D.position.x;
+    let dz = target.object3D.position.z - this.obj.object3D.position.z;
+    this.angle = Math.atan2(dx, dz); 
   }
-  forward(){
-      let dx = this.model.speed * Math.sin(this.angle);
-      let dz = this.model.speed * Math.cos(this.angle);
-      this.obj.object3D.position.x += dx;
-      this.obj.object3D.position.z += dz; 
-      this.obj.setAttribute("animation-mixer",{clip:this.model.charge, timeScale:0.75});
+
+  rotateTowards(target) {
+    this.angleTo(target);
+    this.obj.object3D.rotation.y = this.angle; 
   }
-  stop(){
-    this.obj.setAttribute("animation-mixer",{timeScale:0});
+
+  forward() {
+    let dx = this.speed * Math.sin(this.angle);
+    let dz = this.speed * Math.cos(this.angle);
+    this.obj.object3D.position.x += dx;
+    this.obj.object3D.position.z += dz;
+  }
+
+  chase(target) {
+    const dist = this.distanceTo(target);
+
+    if (dist <= this.attackDistance && !this.isAttacking) {
+      // Attack the player
+      this.isAttacking = true;
+      this.isChasing = false;
+      this.obj.setAttribute("animation-mixer", { clip: "wendigo_jjk_body1_model0_skeleton|Attack 3", loop: "repeat" });
+
+      setTimeout(() => {
+        this.isAttacking = false;
+        this.obj.setAttribute("animation-mixer", { clip: "wendigo_jjk_body1_model0_skeleton|Run", loop: "repeat" });
+      }, 2000); 
+    } else if (dist <= this.chaseDistance && !this.isAttacking) {
+      this.isChasing = true;
+      this.rotateTowards(target);
+      this.forward();
+      this.obj.setAttribute("animation-mixer", { clip: "wendigo_jjk_body1_model0_skeleton|Run", loop: "repeat" });
+    } else {
+      this.isChasing = false;
+      this.startWandering();
+    }
+  }
+
+  distanceTo(target) {
+    const pos1 = this.obj.object3D.position;
+    const pos2 = target.object3D.position;
+    return Math.sqrt(
+      Math.pow(pos1.x - pos2.x, 2) +
+      Math.pow(pos1.y - pos2.y, 2) +
+      Math.pow(pos1.z - pos2.z, 2)
+    );
+  }
+
+  startWandering() {
+    if (this.wanderInterval) return; 
+
+    this.obj.setAttribute("animation-mixer", { clip: "wendigo_jjk_body1_model0_skeleton|Walk", loop: "repeat" });
+    console.log("Monster is wandering!");
+
+    this.wanderInterval = setInterval(() => {
+      this.angle = Math.random() * Math.PI * 2; 
+      this.forward();
+    }, 2000); 
+  }
+
+  stopWandering() {
+    if (this.wanderInterval) {
+      clearInterval(this.wanderInterval);
+      this.wanderInterval = null;
+    }
+  }
+
+  stop() {
+    this.stopWandering();
+    this.obj.setAttribute("animation-mixer", { timeScale: 0 });
   }
 }
